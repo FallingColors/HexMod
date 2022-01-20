5608db3adaf7bf2f78f0e8e7133612edb1bb5ad7
gamma-delta <29877714+gamma-delta@users.noreply.github.com>
2022-01-20 16:52:03 -0600

    fix OpIndex not working

--- src/main/java/at/petrak/hex/common/casting/operators/lists/OpIndex.kt
+++ src/main/java/at/petrak/hex/common/casting/operators/lists/OpIndex.kt
@@ -2,7 +2,6 @@ package at.petrak.hex.common.casting.operators.lists
 
 import at.petrak.hex.api.ConstManaOperator
 import at.petrak.hex.api.Operator.Companion.getChecked
-import at.petrak.hex.api.Operator.Companion.spellListOf
 import at.petrak.hex.api.SpellDatum
 import at.petrak.hex.common.casting.CastingContext
 import at.petrak.hex.common.casting.Widget
@@ -14,6 +13,6 @@ object OpIndex : ConstManaOperator {
         val list = args.getChecked<List<SpellDatum<*>>>(0).toMutableList()
         val index = args.getChecked<Double>(1)
         val x = list.getOrElse(index.roundToInt()) { SpellDatum.make(Widget.NULL) }
-        return spellListOf(x)
+        return listOf(x)
     }
 }
