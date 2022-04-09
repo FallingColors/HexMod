761866336a6bd6b18e06c657929ec4dfd14ea753
yrsegal@gmail.com <yrsegal@gmail.com>
2022-04-08 21:12:39 -0400

    Fix #58

--- src/main/java/at/petrak/hexcasting/common/casting/operators/spells/OpExplode.kt
+++ src/main/java/at/petrak/hexcasting/common/casting/operators/spells/OpExplode.kt
@@ -23,7 +23,7 @@ class OpExplode(val fire: Boolean) : SpellOperator {
         ctx.assertVecInRange(pos)
         return Triple(
             Spell(pos, strength, this.fire),
-            ((1 + strength + if (this.fire) 2 else 0) * 50_000.0).toInt(),
+            ((1 + Mth.clamp(strength.toFloat(), 0f, 10f) + if (this.fire) 2 else 0) * 50_000.0).toInt(),
             listOf(ParticleSpray.Burst(pos, strength, 50))
         )
     }
