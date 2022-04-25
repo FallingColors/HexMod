1e16cfc9e51ffb4616f4c0096f252502c4dfa622
yrsegal@gmail.com <yrsegal@gmail.com>
2022-04-25 19:53:55 -0400

    *screaming louder*

--- src/main/java/at/petrak/hexcasting/common/misc/PlayerPositionRecorder.java
+++ src/main/java/at/petrak/hexcasting/common/misc/PlayerPositionRecorder.java
@@ -1,9 +1,10 @@
 package at.petrak.hexcasting.common.misc;
 
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.Vec3;
-import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 import java.util.Map;
@@ -13,9 +14,11 @@ public final class PlayerPositionRecorder {
 	private static final Map<Player, Vec3> LAST_POSITION_MAP = new WeakHashMap<>();
 
 	@SubscribeEvent
-	public static void onEntityUpdate(LivingEvent.LivingUpdateEvent evt) {
-		if (evt.getEntityLiving() instanceof ServerPlayer player) {
-			LAST_POSITION_MAP.put(player, player.position());
+	public static void onEntityUpdate(TickEvent.WorldTickEvent evt) {
+		if (evt.phase == TickEvent.Phase.END && evt.world instanceof ServerLevel world) {
+			for (ServerPlayer player : world.players()) {
+				LAST_POSITION_MAP.put(player, player.position());
+			}
 		}
 	}
 
