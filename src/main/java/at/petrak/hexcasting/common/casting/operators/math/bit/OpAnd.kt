8878dc40484410306375e22bc14f6b8aef85facb
yrsegal@gmail.com <yrsegal@gmail.com>
2022-04-28 13:16:01 -0400

    better mishap messages for bitwise ops

--- src/main/java/at/petrak/hexcasting/common/casting/operators/math/bit/OpAnd.kt
+++ src/main/java/at/petrak/hexcasting/common/casting/operators/math/bit/OpAnd.kt
@@ -1,26 +1,26 @@
 package at.petrak.hexcasting.common.casting.operators.math.bit
 
 import at.petrak.hexcasting.api.spell.ConstManaOperator
-import at.petrak.hexcasting.api.spell.DatumType
 import at.petrak.hexcasting.api.spell.Operator.Companion.getChecked
 import at.petrak.hexcasting.api.spell.Operator.Companion.spellListOf
 import at.petrak.hexcasting.api.spell.SpellDatum
 import at.petrak.hexcasting.api.spell.casting.CastingContext
+import at.petrak.hexcasting.common.casting.operators.math.MathOpUtils
 import kotlin.math.roundToInt
 
 object OpAnd : ConstManaOperator {
     override val argc = 2
 
     override fun execute(args: List<SpellDatum<*>>, ctx: CastingContext): List<SpellDatum<*>> {
-        val type = args[0].getType()
+        val firstParam = MathOpUtils.GetNumOrList(args[0], 0)
 
-        if (type == DatumType.LIST) {
-            val list1 = args.getChecked<List<SpellDatum<*>>>(0)
+        if (firstParam.right().isPresent) {
+            val list1 = firstParam.right().get()
             val list2 = args.getChecked<List<SpellDatum<*>>>(1)
             return spellListOf(list1.filter { it in list2 })
         }
 
-        val num1 = args.getChecked<Double>(0).roundToInt()
+        val num1 = firstParam.left().get().roundToInt()
         val num2 = args.getChecked<Double>(1).roundToInt()
         return spellListOf((num1 and num2).toDouble())
     }
