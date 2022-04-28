1c82fe482e303f28c29fd81c4a39c66304729dc6
yrsegal@gmail.com <yrsegal@gmail.com>
2022-04-28 12:04:33 -0400

    add slice to the book

--- Common/src/main/java/at/petrak/hexcasting/common/casting/operators/lists/OpSlice.kt
+++ Common/src/main/java/at/petrak/hexcasting/common/casting/operators/lists/OpSlice.kt
@@ -5,7 +5,7 @@ import at.petrak.hexcasting.api.spell.Operator.Companion.getChecked
 import at.petrak.hexcasting.api.spell.Operator.Companion.spellListOf
 import at.petrak.hexcasting.api.spell.SpellDatum
 import at.petrak.hexcasting.api.spell.casting.CastingContext
-import at.petrak.hexcasting.api.spell.Widget
+import net.minecraft.util.Mth
 import kotlin.math.max
 import kotlin.math.min
 import kotlin.math.roundToInt
@@ -14,9 +14,12 @@ object OpSlice : ConstManaOperator {
     override val argc = 3
     override fun execute(args: List<SpellDatum<*>>, ctx: CastingContext): List<SpellDatum<*>> {
         val list = args.getChecked<List<SpellDatum<*>>>(0)
-        val index1 = max(0, args.getChecked<Double>(1).roundToInt())
-        val index2 = min(args.getChecked<Double>(2).roundToInt(), list.size)
+        val index1 = Mth.clamp(args.getChecked<Double>(1).roundToInt(), 0, list.size)
+        val index2 = Mth.clamp(args.getChecked<Double>(2).roundToInt(), 0, list.size)
 
-        return spellListOf(list.subList(index1, index2))
+        if (index1 == index2)
+            return spellListOf(listOf<SpellDatum<*>>())
+
+        return spellListOf(list.subList(min(index1, index2), max(index1, index2)))
     }
 }
