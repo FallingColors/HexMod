84a8d5deb7d93edbee7b4745e5f29c79ee3778d2
yrsegal@gmail.com <yrsegal@gmail.com>
2022-05-05 19:07:33 -0400

    change advancement to trigger on charged amethyst

--- Common/src/main/java/at/petrak/hexcasting/common/casting/operators/OpBlockAxisRaycast.kt
+++ Common/src/main/java/at/petrak/hexcasting/common/casting/operators/OpBlockAxisRaycast.kt
@@ -5,15 +5,15 @@ import at.petrak.hexcasting.api.spell.ConstManaOperator
 import at.petrak.hexcasting.api.spell.Operator
 import at.petrak.hexcasting.api.spell.Operator.Companion.getChecked
 import at.petrak.hexcasting.api.spell.SpellDatum
-import at.petrak.hexcasting.api.spell.casting.CastingContext
 import at.petrak.hexcasting.api.spell.Widget
+import at.petrak.hexcasting.api.spell.casting.CastingContext
 import net.minecraft.world.level.ClipContext
 import net.minecraft.world.phys.HitResult
 import net.minecraft.world.phys.Vec3
 
 object OpBlockAxisRaycast : ConstManaOperator {
     override val argc = 2
-    override val manaCost = ManaConstants.DUST_UNIT / 1000
+    override val manaCost = ManaConstants.DUST_UNIT / 100
     override fun execute(args: List<SpellDatum<*>>, ctx: CastingContext): List<SpellDatum<*>> {
         val origin: Vec3 = args.getChecked(0)
         val look: Vec3 = args.getChecked(1)
