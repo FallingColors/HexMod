c77e38b5da3165b5c3e21e5952e034bc783c9f7b
Alwinfy <20421383+Alwinfy@users.noreply.github.com>
2022-05-08 21:06:14 -0400

    Make empty lists falsy

--- Common/src/main/java/at/petrak/hexcasting/common/casting/operators/math/logic/OpBoolIdentityKindOf.kt
+++ Common/src/main/java/at/petrak/hexcasting/common/casting/operators/math/logic/OpBoolIdentityKindOf.kt
@@ -3,6 +3,7 @@ package at.petrak.hexcasting.common.casting.operators.math.logic
 import at.petrak.hexcasting.api.spell.ConstManaOperator
 import at.petrak.hexcasting.api.spell.Operator.Companion.spellListOf
 import at.petrak.hexcasting.api.spell.SpellDatum
+import at.petrak.hexcasting.api.spell.SpellList
 import at.petrak.hexcasting.api.spell.casting.CastingContext
 import at.petrak.hexcasting.api.spell.Widget
 import net.minecraft.world.phys.Vec3
@@ -12,10 +13,11 @@ object OpBoolIdentityKindOf : ConstManaOperator {
 
     override fun execute(args: List<SpellDatum<*>>, ctx: CastingContext): List<SpellDatum<*>> {
         return spellListOf(
-            when (args[0].payload) {
+            when (val payload = args[0].payload) {
                 Widget.NULL -> 0.0
                 0.0, Vec3.ZERO -> Widget.NULL
-                else -> args[0].payload
+		is SpellList -> if (payload.nonEmpty) payload else Widget.NULL
+                else -> payload
             }
         )
     }
