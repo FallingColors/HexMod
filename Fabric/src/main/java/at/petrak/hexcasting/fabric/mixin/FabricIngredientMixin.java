935c7dd9497ced4868ccdc178ea6f8f0b72ae986
gamma-delta <29877714+gamma-delta@users.noreply.github.com>
2022-05-31 12:30:06 -0500

    undo blanketcon hotfix so we have a good version on CI

--- Fabric/src/main/java/at/petrak/hexcasting/fabric/mixin/FabricIngredientMixin.java
+++ Fabric/src/main/java/at/petrak/hexcasting/fabric/mixin/FabricIngredientMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value = Ingredient.class, priority = 4000)
+@Mixin(value = Ingredient.class)
 public class FabricIngredientMixin {
     @Inject(method = "fromJson", at = @At("HEAD"), cancellable = true)
     private static void fromJson(JsonElement jsonElement, CallbackInfoReturnable<Ingredient> cir) {
