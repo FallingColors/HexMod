2e76655d210ec7fedffe8fe04fb4aded161aa09c
yrsegal@gmail.com <yrsegal@gmail.com>
2022-07-03 00:00:10 -0400

    Close #110

--- Common/src/main/java/at/petrak/hexcasting/common/casting/operators/spells/great/OpTeleport.kt
+++ Common/src/main/java/at/petrak/hexcasting/common/casting/operators/spells/great/OpTeleport.kt
@@ -55,7 +55,7 @@ object OpTeleport : SpellAction {
                 }
             }
 
-            if (teleportee is ServerPlayer) {
+            if (teleportee is ServerPlayer && teleportee == ctx.caster) {
                 // Drop items conditionally, based on distance teleported.
                 // MOST IMPORTANT: Never drop main hand item, since if it's a trinket, it will get duplicated later.
 
