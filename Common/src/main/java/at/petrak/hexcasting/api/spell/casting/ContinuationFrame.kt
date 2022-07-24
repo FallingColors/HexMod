e404aa7c2e25e4712351485d70f3044b4ba1b49d
yrsegal@gmail.com <yrsegal@gmail.com>
2022-07-24 19:54:14 -0400

    fix #174

--- Common/src/main/java/at/petrak/hexcasting/api/spell/casting/ContinuationFrame.kt
+++ Common/src/main/java/at/petrak/hexcasting/api/spell/casting/ContinuationFrame.kt
@@ -146,7 +146,9 @@ sealed interface ContinuationFrame {
 
             // If we still have data to process...
             val (stackTop, newCont) = if (data.nonEmpty) {
-                // Push the next datum to the top of the stack,
+                // Increment the evaluation depth,
+                harness.ctx.incDepth()
+                // push the next datum to the top of the stack,
                 data.car to continuation
                     // put the next Thoth object back on the stack for the next Thoth cycle,
                     .pushFrame(ForEach(data.cdr, code, stack, acc))
