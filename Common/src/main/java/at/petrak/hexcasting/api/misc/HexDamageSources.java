440ef4d0a4167810bbb5323d60fcc615e5bb0fd5
gamma-delta <29877714+gamma-delta@users.noreply.github.com>
2022-11-13 13:05:31 -0600

    close #271

--- Common/src/main/java/at/petrak/hexcasting/api/misc/HexDamageSources.java
+++ Common/src/main/java/at/petrak/hexcasting/api/misc/HexDamageSources.java
@@ -4,7 +4,8 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.Entity;
 
 public final class HexDamageSources {
-    public static final DamageSource OVERCAST = new DamageSourceOvercast();
+    public static final DamageSourceOvercast OVERCAST = new DamageSourceOvercast();
+    public static final DamageSourceShameOnYou SHAME = new DamageSourceShameOnYou();
 
     public static DamageSource overcastDamageFrom(Entity cause) {
         return new EntityDamageSourceOvercast(cause);
--- Common/src/main/java/at/petrak/hexcasting/common/casting/operators/stack/OpDuplicateN.kt
+++ Common/src/main/java/at/petrak/hexcasting/common/casting/operators/stack/OpDuplicateN.kt
@@ -4,6 +4,7 @@ import at.petrak.hexcasting.api.spell.ConstMediaAction
 import at.petrak.hexcasting.api.spell.casting.CastingContext
 import at.petrak.hexcasting.api.spell.getPositiveInt
 import at.petrak.hexcasting.api.spell.iota.Iota
+import at.petrak.hexcasting.api.spell.mishaps.MishapShameOnYou
 
 object OpDuplicateN : ConstMediaAction {
     override val argc: Int
@@ -11,6 +12,10 @@ object OpDuplicateN : ConstMediaAction {
 
     override fun execute(args: List<Iota>, ctx: CastingContext): List<Iota> {
         val count = args.getPositiveInt(1, argc)
+
+        if (count > 1000) {
+            throw MishapShameOnYou()
+        }
 
         return (List(count) { args[0] })
     }
