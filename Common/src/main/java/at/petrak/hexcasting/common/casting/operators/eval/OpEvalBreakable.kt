7467687fad911e7d01f8ff85cd837597239b109e
Alwinfy <20421383+Alwinfy@users.noreply.github.com>
2022-11-16 00:58:29 -0500

    DRY eval/cc

--- Common/src/main/java/at/petrak/hexcasting/common/casting/operators/eval/OpEvalBreakable.kt
+++ Common/src/main/java/at/petrak/hexcasting/common/casting/operators/eval/OpEvalBreakable.kt
@@ -17,25 +17,7 @@ object OpEvalBreakable : Action {
         ravenmind: Iota?,
         ctx: CastingContext
     ): OperationResult {
-        val datum = stack.removeLast()
-        val instrs = evaluatable(datum, 0)
-
-        instrs.ifRight {
-            ctx.incDepth()
-        }
-
-        // if not installed already...
-        // also, never make a break boundary when evaluating just one pattern
-        val newCont =
-            if (instrs.left().isPresent || (continuation is SpellContinuation.NotDone && continuation.frame is ContinuationFrame.FinishEval)) {
-                continuation
-            } else {
-                continuation.pushFrame(ContinuationFrame.FinishEval) // install a break-boundary after eval
-            }
-
-        val instrsList = instrs.map({ SpellList.LList(0, listOf(it)) }, { it })
-        val frame = ContinuationFrame.Evaluate(instrsList)
         stack.add(ContinuationIota(continuation))
-        return OperationResult(newCont.pushFrame(frame), stack, ravenmind, listOf())
+        return OpEval.operate(continuation, stack, ravenmind, ctx)
     }
 }
