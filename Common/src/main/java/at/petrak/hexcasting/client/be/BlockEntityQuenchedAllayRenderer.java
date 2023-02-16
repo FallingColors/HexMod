ac9e00f675a800257823df50a566186f57854ab1
petrak@ <petrathekat@gmail.com>
2023-02-16 13:53:37 -0600

    gas my own light, gate my own keep, and girl my own boss

--- Common/src/main/java/at/petrak/hexcasting/client/be/BlockEntityQuenchedAllayRenderer.java
+++ Common/src/main/java/at/petrak/hexcasting/client/be/BlockEntityQuenchedAllayRenderer.java
@@ -3,12 +3,14 @@ package at.petrak.hexcasting.client.be;
 import at.petrak.hexcasting.client.RegisterClientStuff;
 import at.petrak.hexcasting.common.blocks.BlockQuenchedAllay;
 import at.petrak.hexcasting.common.blocks.entity.BlockEntityQuenchedAllay;
+import at.petrak.hexcasting.xplat.IClientXplatAbstractions;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.block.BlockRenderDispatcher;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.world.phys.AABB;
 
 public class BlockEntityQuenchedAllayRenderer implements BlockEntityRenderer<BlockEntityQuenchedAllay> {
     private static int GASLIGHTING_AMOUNT = 0;
@@ -36,7 +38,19 @@ public class BlockEntityQuenchedAllayRenderer implements BlockEntityRenderer<Blo
     @Override
     public void render(BlockEntityQuenchedAllay blockEntity, float partialTick, PoseStack poseStack,
         MultiBufferSource bufferSource, int packedLight, int packedOverlay) {
-        doRender(this.ctx.getBlockRenderDispatcher(), poseStack, bufferSource, packedLight, packedOverlay);
+        // https://github.com/MinecraftForge/MinecraftForge/blob/79dfdb0ace9694f9dd0f1d9e8c5c24a0ae2d77f8/patches/minecraft/net/minecraft/client/renderer/LevelRenderer.java.patch#L68
+        // Forge fixes BEs rendering offscreen; Fabric doesn't!
+        // So we do a special check on Fabric only
+        var pos = blockEntity.getBlockPos();
+        var aabb = new AABB(pos.offset(-1, 0, -1), pos.offset(1, 1, 1));
+        if (IClientXplatAbstractions.INSTANCE.fabricAdditionalQuenchFrustumCheck(aabb)) {
+            doRender(this.ctx.getBlockRenderDispatcher(), poseStack, bufferSource, packedLight, packedOverlay);
+        }
+    }
+
+    @Override
+    public boolean shouldRenderOffScreen(BlockEntityQuenchedAllay blockEntity) {
+        return false;
     }
 
     public static int getGaslightingAmount() {
