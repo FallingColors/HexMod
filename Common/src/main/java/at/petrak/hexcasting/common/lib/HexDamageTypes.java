97244cd4618ed0418c7a0b317daa3f6f57034b83
Talia-12 <paradox.m.burgess@gmail.com>
2023-07-30 16:30:12 +1000

    fix added damage types

--- Common/src/main/java/at/petrak/hexcasting/common/lib/HexDamageTypes.java
+++ Common/src/main/java/at/petrak/hexcasting/common/lib/HexDamageTypes.java
@@ -1,17 +1,12 @@
 package at.petrak.hexcasting.common.lib;
 
 import net.minecraft.core.registries.Registries;
-import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.damagesource.DamageType;
 
-public class HexDamageTypes {
-    public static final ResourceKey<DamageType> OVERCAST = ResourceKey.create(Registries.DAMAGE_TYPE, new ResourceLocation("overcast"));
-    public static final ResourceKey<DamageType> SHAME_ON_YOU = ResourceKey.create(Registries.DAMAGE_TYPE, new ResourceLocation("overcast"));
+import static at.petrak.hexcasting.api.HexAPI.modLoc;
 
-    public static void bootstrap(BootstapContext<DamageType> context) {
-        context.register(OVERCAST, new DamageType("hexcasting.overcast", 0f));
-        context.register(SHAME_ON_YOU, new DamageType("hexcasting.shame", 0f));
-    }
+public class HexDamageTypes {
+    public static final ResourceKey<DamageType> OVERCAST = ResourceKey.create(Registries.DAMAGE_TYPE, modLoc("overcast"));
+    public static final ResourceKey<DamageType> SHAME_ON_YOU = ResourceKey.create(Registries.DAMAGE_TYPE, modLoc("overcast"));
 }
