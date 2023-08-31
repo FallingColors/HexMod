ce3eec4dd456ba21b2a85686245c77b3e6c7ca3d
object-Object <object@objectobject.ca>
2023-08-31 03:16:25 -0400

    Fix release argument in build

--- doc/test/test_snapshots.py
+++ doc/test/test_snapshots.py
@@ -13,7 +13,7 @@ def test_file(tmp_path: Path, snapshot: SnapshotAssertion):
     # generate output docs html file and assert it hasn't changed vs. the snapshot
     main(
         Args.parse_args(
-            [PROPS, "--lang", "en_us", "--release", "-o", tmp_path.as_posix()]
+            [PROPS, "--lang", "en_us", "--release=true", "-o", tmp_path.as_posix()]
         )
     )
     out_path = tmp_path / "index.html"
@@ -23,7 +23,15 @@ def test_file(tmp_path: Path, snapshot: SnapshotAssertion):
 def test_cmd(tmp_path: Path, snapshot: SnapshotAssertion):
     # as above, but running the command we actually want to be using
     subprocess.run(
-        ["hexdoc", PROPS, "--lang", "en_us", "--release", "-o", tmp_path.as_posix()],
+        [
+            "hexdoc",
+            PROPS,
+            "--lang",
+            "en_us",
+            "--release=true",
+            "-o",
+            tmp_path.as_posix(),
+        ],
         stdout=sys.stdout,
         stderr=sys.stderr,
     )
