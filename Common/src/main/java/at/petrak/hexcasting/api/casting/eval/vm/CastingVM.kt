6dd278798ecef3aff9a76fd2ea2805d26625c261
Talia-12 <paradox.m.burgess@gmail.com>
2024-03-16 23:06:23 +1000

    Update CastingVM.kt

--- Common/src/main/java/at/petrak/hexcasting/api/casting/eval/vm/CastingVM.kt
+++ Common/src/main/java/at/petrak/hexcasting/api/casting/eval/vm/CastingVM.kt
@@ -64,8 +64,7 @@ class CastingVM(var image: CastingImage, val env: CastingEnvironment) {
                 performSideEffects(info, image2.sideEffects)
             } catch (e: Exception) {
                 e.printStackTrace()
-                val javamishap = MishapInternalException(e)
-                performSideEffects(info,listOf(OperatorSideEffect.DoMishap(javamishap,Mishap.Context(null,null))))
+                performSideEffects(info, listOf(OperatorSideEffect.DoMishap(MishapInternalException(e), Mishap.Context(null, null))))
             }
             info.earlyExit = info.earlyExit || !lastResolutionType.success
         }
