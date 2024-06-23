114d2f2676051051041e631c469c4b88acaf6131
object-Object <object@objectobject.ca>
2024-06-23 15:55:51 -0400

    Implement datagen for damage type definitions and tags

--- Common/src/main/java/at/petrak/hexcasting/common/lib/HexDamageTypes.java
+++ Common/src/main/java/at/petrak/hexcasting/common/lib/HexDamageTypes.java
@@ -1,7 +1,9 @@
 package at.petrak.hexcasting.common.lib;
 
 import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstapContext;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.damagesource.DamageScaling;
 import net.minecraft.world.damagesource.DamageType;
 
 import static at.petrak.hexcasting.api.HexAPI.modLoc;
@@ -9,4 +11,18 @@ import static at.petrak.hexcasting.api.HexAPI.modLoc;
 public class HexDamageTypes {
     public static final ResourceKey<DamageType> OVERCAST = ResourceKey.create(Registries.DAMAGE_TYPE, modLoc("overcast"));
     public static final ResourceKey<DamageType> SHAME_ON_YOU = ResourceKey.create(Registries.DAMAGE_TYPE, modLoc("shame"));
+
+    public static void bootstrap(BootstapContext<DamageType> ctx) {
+        ctx.register(OVERCAST, new DamageType(
+            "hexcasting.overcast",
+            DamageScaling.WHEN_CAUSED_BY_LIVING_NON_PLAYER,
+            0f
+        ));
+
+        ctx.register(SHAME_ON_YOU, new DamageType(
+            "hexcasting.shame",
+            DamageScaling.WHEN_CAUSED_BY_LIVING_NON_PLAYER,
+            0f
+        ));
+    }
 }
