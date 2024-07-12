a74887922d2eb1dde131d2d1408638a7706cc76a
SamsTheNerd <sam@samsthenerd.com>
2024-07-12 18:59:44 -0400

    add chat feedback to texture debug commands

--- Common/src/main/java/at/petrak/hexcasting/common/command/PatternTexturesCommand.java
+++ Common/src/main/java/at/petrak/hexcasting/common/command/PatternTexturesCommand.java
@@ -5,27 +5,34 @@ import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.Component;
 
 public class PatternTexturesCommand
 {
     public static void add(LiteralArgumentBuilder<CommandSourceStack> cmd) {
+        // TODO: do we want these in release ??
         cmd.then(Commands.literal("textureToggle")
                 .requires(dp -> dp.hasPermission(Commands.LEVEL_ADMINS))
                 .executes(ctx -> {
                     PatternTextureManager.useTextures = !PatternTextureManager.useTextures;
+                    String log = (PatternTextureManager.useTextures ? "Enabled" : "Disabled") + " pattern texture rendering. This is meant for debugging.";
+                    ctx.getSource().sendSuccess(() -> Component.literal(log), true);
                     return 1;
                 }));
         cmd.then(Commands.literal("textureRepaint")
                 .requires(dp -> dp.hasPermission(Commands.LEVEL_ADMINS))
                 .executes(ctx -> {
                     PatternTextureManager.repaint();
+                    ctx.getSource().sendSuccess(() -> Component.literal("Repainting pattern textures. This is meant for debugging."), true);
                     return 1;
                 }));
         cmd.then(Commands.literal("textureSetResolutionScaler")
                 .requires(dp -> dp.hasPermission(Commands.LEVEL_ADMINS))
                 .then(Commands.argument("integer", IntegerArgumentType.integer()).executes(ctx -> {
-                    PatternTextureManager.setResolutionScaler(IntegerArgumentType.getInteger(ctx, "integer"));
+                    int newRes = IntegerArgumentType.getInteger(ctx, "integer");
+                    PatternTextureManager.setResolutionScaler(newRes);
                     PatternTextureManager.repaint();
+                    ctx.getSource().sendSuccess(() -> Component.literal("Setting pattern resolution scaler to " + newRes + ". This is meant for debugging."), true);
                     return 1;
                 })));
     }
