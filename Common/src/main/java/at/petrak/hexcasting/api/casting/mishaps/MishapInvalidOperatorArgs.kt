652c1006a18658bec55ce4870ef010e81e9160eb
kineticneticat <84404038+kineticneticat@users.noreply.github.com>
2024-07-12 22:46:59 +0100

    do all the things

--- Common/src/main/java/at/petrak/hexcasting/api/casting/mishaps/MishapInvalidOperatorArgs.kt
+++ Common/src/main/java/at/petrak/hexcasting/api/casting/mishaps/MishapInvalidOperatorArgs.kt
@@ -4,17 +4,15 @@ import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
 import at.petrak.hexcasting.api.casting.iota.GarbageIota
 import at.petrak.hexcasting.api.casting.iota.Iota
 import at.petrak.hexcasting.api.pigment.FrozenPigment
+import at.petrak.hexcasting.api.utils.asTextComponent
 import net.minecraft.network.chat.Component
-import net.minecraft.network.chat.ComponentContents
-import net.minecraft.network.chat.MutableComponent
+import net.minecraft.network.chat.ComponentUtils
 import net.minecraft.world.item.DyeColor
 
 /**
  * The value failed some kind of predicate.
  */
-class MishapInvalidOperatorArgs(
-    private val perpetrators: List<Iota>
-) : Mishap() {
+class MishapInvalidOperatorArgs( val perpetrators: List<Iota> ) : Mishap() {
     override fun accentColor(ctx: CastingEnvironment, errorCtx: Context): FrozenPigment =
         dyeColor(DyeColor.GRAY)
 
@@ -27,28 +25,18 @@ class MishapInvalidOperatorArgs(
     override fun errorMessage(ctx: CastingEnvironment, errorCtx: Context): Component {
         return if (perpetrators.size == 1) {
             error(
-                "invalid_operator_args.single",
+                "invalid_operator_args.one",
                 0,
                 perpetrators[0].display()
             )
         } else {
             error(
-                "invalid_operator_args.plural",
+                "invalid_operator_args.many",
                 perpetrators.size,
                 0,
                 perpetrators.lastIndex,
-                collateIotas(perpetrators)
+                ComponentUtils.formatList(perpetrators.map {it.display()}, ", ".asTextComponent)
             )
         }
-    }
-    private fun collateIotas(iotas: List<Iota>): MutableComponent {
-        val out = MutableComponent.create(ComponentContents.EMPTY)
-        for (i in iotas.indices) {
-            out.append(iotas[i].display())
-            if (i < iotas.size-1) {
-                out.append(", ")
-            }
-        }
-        return out
     }
 }
