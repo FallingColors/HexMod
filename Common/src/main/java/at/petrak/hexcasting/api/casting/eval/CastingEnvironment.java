d6e11f8741043338283f956dce71ec21e7dc05b9
Walker Fowlkes <walkerffo22@gmail.com>
2024-07-13 11:17:33 -0700

    remove dead import

--- Common/src/main/java/at/petrak/hexcasting/api/casting/eval/CastingEnvironment.java
+++ Common/src/main/java/at/petrak/hexcasting/api/casting/eval/CastingEnvironment.java
@@ -30,7 +30,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BiConsumer;
-import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 import static at.petrak.hexcasting.api.HexAPI.modLoc;
