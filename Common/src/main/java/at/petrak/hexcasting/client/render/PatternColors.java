1c339fc80e963d3573366f34b1548ba916db642e
SamsTheNerd <sam@samsthenerd.com>
2024-07-16 17:36:15 -0400

    add dot colors

--- Common/src/main/java/at/petrak/hexcasting/client/render/PatternColors.java
+++ Common/src/main/java/at/petrak/hexcasting/client/render/PatternColors.java
@@ -12,19 +12,32 @@ public class PatternColors {
     protected final int outerStartColor;
     protected final int outerEndColor;
 
-    public PatternColors(int innerStartColor, int innerEndColor, int outerStartColor, int outerEndColor){
+    protected final int startingDotColor;
+    protected final int gridDotsColor;
+
+    public PatternColors(int innerStartColor, int innerEndColor, int outerStartColor, int outerEndColor,
+        int startingDotsColor, int gridDotsColor){
         this.innerStartColor = innerStartColor;
         this.innerEndColor = innerEndColor;
         this.outerStartColor = outerStartColor;
         this.outerEndColor = outerEndColor;
+        this.startingDotColor = startingDotsColor;
+        this.gridDotsColor = gridDotsColor;
     }
 
+    // no gradient
     public PatternColors(int innerColor, int outerColor){
-        this(innerColor, innerColor, outerColor, outerColor);
+        this(innerColor, innerColor, outerColor, outerColor, 0, 0);
     }
 
-    // single color
+    // single color -- no outer layer
     public PatternColors(int color){
         this(color, 0);
+    }
+
+    // add dots -- note, this is how you tell the renderer to make dots
+    public PatternColors withDotColors(int startingDotColor, int gridDotsColor){
+        return new PatternColors(this.innerStartColor, this.innerEndColor, this.outerStartColor, this.outerEndColor,
+                startingDotColor, gridDotsColor);
     }
 }
