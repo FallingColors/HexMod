4ba222b335707665769b78807130d632b86dee2f
SamsTheNerd <sam@samsthenerd.com>
2024-07-18 01:11:18 -0400

    clean up worldly renderers - get rid of icky ps transformer stuff

--- Common/src/main/java/at/petrak/hexcasting/client/render/WorldlyPatternRenderHelpers.java
+++ Common/src/main/java/at/petrak/hexcasting/client/render/WorldlyPatternRenderHelpers.java
@@ -1,21 +1,20 @@
 package at.petrak.hexcasting.client.render;
 
-import at.petrak.hexcasting.api.block.HexBlockEntity;
 import at.petrak.hexcasting.api.casting.math.HexPattern;
 import at.petrak.hexcasting.common.blocks.akashic.BlockAkashicBookshelf;
 import at.petrak.hexcasting.common.blocks.akashic.BlockEntityAkashicBookshelf;
 import at.petrak.hexcasting.common.blocks.circles.BlockEntitySlate;
 import at.petrak.hexcasting.common.blocks.circles.BlockSlate;
 import at.petrak.hexcasting.common.entities.EntityWallScroll;
-import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
-import net.minecraft.client.renderer.GameRenderer;
-import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.core.Vec3i;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.AttachFace;
 import net.minecraft.world.phys.Vec3;
+
+import javax.annotation.Nullable;
 
 /**
  * Helper methods for rendering patterns in the world.
@@ -51,11 +50,17 @@ public class WorldlyPatternRenderHelpers {
     public static void renderPatternForScroll(HexPattern pattern, EntityWallScroll scroll, PoseStack ps, MultiBufferSource bufSource, int light, int blockSize, boolean showStrokeOrder)
     {
         ps.pushPose();
+        ps.translate(-blockSize / 2f, -blockSize / 2f, 1f / 32f);
         renderPattern(pattern, showStrokeOrder ? READABLE_SCROLL_RENDER_SETTINGS : SCROLL_RENDER_SETTINGS,
                 showStrokeOrder ? READABLE_SCROLL_COLORS : DEFAULT_PATTERN_COLOR,
-                scroll.getPos().hashCode(), ps, bufSource, light, blockSize, true, false,false, true,-1);
+                scroll.getPos().hashCode(), ps, bufSource, null, null, light, blockSize);
         ps.popPose();
     }
+
+    private static final int[] WALL_ROTATIONS = {180, 270, 0, 90};
+    private static final Vec3i[] SLATE_FACINGS = {new Vec3i(0, -1, 0), new Vec3i(-1, -1, 0), new Vec3i(-1, -1, 1), new Vec3i(0, -1 , 1)};
+    private static final Vec3[] WALL_NORMALS = {new Vec3(0, 0, -1), new Vec3(-1, 0, 0), new Vec3(0, 0, -1), new Vec3(-1, 0, 0)};
+    private static final Vec3i[] SLATE_FLOORCEIL_FACINGS = {new Vec3i(0,0,0), new Vec3i(1,0,0), new Vec3i(1,0,1), new Vec3i(0,0,1)};
 
     public static void renderPatternForSlate(BlockEntitySlate tile, HexPattern pattern, PoseStack ps, MultiBufferSource buffer, int light, BlockState bs)
     {
@@ -66,179 +71,73 @@ public class WorldlyPatternRenderHelpers {
 
         boolean wombly = bs.getValue(BlockSlate.ENERGIZED);
 
-        renderPatternForBlockEntity(pattern, tile,
+        ps.pushPose();
+
+        Vec3 normal = null;
+        if(isOnWall){
+            ps.mulPose(Axis.ZP.rotationDegrees(180));
+            Vec3i tV = SLATE_FACINGS[facing % 4];
+            ps.translate(tV.getX(), tV.getY(), tV.getZ());
+            ps.mulPose(Axis.YP.rotationDegrees(WALL_ROTATIONS[facing % 4]));
+            normal = WALL_NORMALS[facing % 4];
+        } else {
+            Vec3i tV = SLATE_FLOORCEIL_FACINGS[facing % 4];
+            ps.translate(tV.getX(), tV.getY(), tV.getZ());
+
+            ps.mulPose(Axis.YP.rotationDegrees(facing*-90));
+            ps.mulPose(Axis.XP.rotationDegrees(90 * (isOnCeiling ? -1 : 1)));
+            if(isOnCeiling) ps.translate(0,-1,1);
+
+            normal = new Vec3(0, 0, -1);
+        }
+
+        renderPattern(pattern,
                 wombly ? SLATE_WOMBLY_SETTINGS : WORLDLY_RENDER_SETTINGS,
                 wombly ? SLATE_WOBBLY_COLOR : DEFAULT_PATTERN_COLOR,
-                ps, buffer, light, isOnWall, isOnCeiling, true, facing);
+                tile.getBlockPos().hashCode(), ps, buffer, normal, null, light, 1);
+        ps.popPose();
     }
+
+    private static final Vec3i[] BLOCK_FACINGS = {new Vec3i(0, -1, 1), new Vec3i(0, -1, 0), new Vec3i(-1, -1, 0), new Vec3i(-1, -1, 1)};
 
     public static void renderPatternForAkashicBookshelf(BlockEntityAkashicBookshelf tile, HexPattern pattern, PoseStack ps, MultiBufferSource buffer, int light, BlockState bs)
     {
+
         int facing = bs.getValue(BlockAkashicBookshelf.FACING).get2DDataValue();
-        renderPatternForBlockEntity(pattern, tile, WORLDLY_RENDER_SETTINGS, DEFAULT_PATTERN_COLOR, ps, buffer, LightTexture.FULL_BRIGHT, true, false, false, facing);
-    }
 
-    public static void renderPatternForBlockEntity(HexPattern pattern, HexBlockEntity tile, PatternRenderSettings patSets, PatternColors patColors, PoseStack ps, MultiBufferSource buffer, int light, boolean isOnWall, boolean isOnCeiling, boolean isSlate, int facing)
-    {
-        var oldShader = RenderSystem.getShader();
         ps.pushPose();
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        renderPattern(pattern, patSets, patColors, tile.getBlockPos().hashCode(), ps, buffer, light, 1, isOnWall, isOnCeiling, isSlate, false, facing);
-        ps.popPose();
-        RenderSystem.setShader(() -> oldShader);
-    }
+        ps.mulPose(Axis.ZP.rotationDegrees(180));
 
-    // TODO: clean up the args of this method, maybe make the whole PSTransformer thing less gross, move it to be just code in the methods or something maybe.
+        Vec3i tV = BLOCK_FACINGS[facing % 4];
+        ps.translate(tV.getX(), tV.getY(), tV.getZ());
+        ps.mulPose(Axis.YP.rotationDegrees(WALL_ROTATIONS[facing % 4]));
+
+        renderPattern(pattern, WORLDLY_RENDER_SETTINGS, DEFAULT_PATTERN_COLOR,
+                tile.getBlockPos().hashCode(), ps, buffer, WALL_NORMALS[facing % 4], -0.02f, light, 1);
+        ps.popPose();
+    }
 
     /**
      * Renders a pattern in world space based on the given transform requirements
      */
-    public static void renderPattern(HexPattern pattern, PatternRenderSettings patSets, PatternColors patColors, double seed, PoseStack ps, MultiBufferSource bufSource, int light, int blockSize, boolean isOnWall, boolean isOnCeiling, boolean isSlate, boolean isScroll, int facing)
+    public static void renderPattern(HexPattern pattern, PatternRenderSettings patSets, PatternColors patColors,
+        double seed, PoseStack ps, MultiBufferSource bufSource, Vec3 normal, @Nullable Float zOffset,
+        int light, int blockSize)
     {
+
         ps.pushPose();
 
 
-        float z = (-1f / 16f) - 0.01f;
+        float z = zOffset != null ? zOffset : ((-1f / 16f) - 0.01f);
 
-        PSTransformer transformer;
-
-        if(isOnWall)
-        {
-            if(isScroll)
-            {
-                transformer = wallScrollTransformer;
-            }
-            else
-            {
-                if(isSlate)
-                {
-                    transformer = wallSlateTransformer;
-                }
-                else
-                {
-                    transformer = wallBlockTransformer;
-                    z = -0.02f;
-                }
-            }
-        }
-        else //slates on the floor or ceiling
-        {
-            transformer = isOnCeiling ? ceilSlateTransformer : floorSlateTransformer;
-        }
-
-        Vec3 nVec = transformer.transform(ps, facing, blockSize);
+        normal = normal != null ? normal : new Vec3(0, 0, -1);
 
         ps.scale(blockSize, blockSize, 1);
         ps.translate(0,0, z);
 
-        PatternRenderer.renderPattern(pattern, ps, new PatternRenderer.WorldlyBits(bufSource, light, nVec),
+        PatternRenderer.renderPattern(pattern, ps, new PatternRenderer.WorldlyBits(bufSource, light, normal),
                 patSets, patColors, seed, blockSize * PatternTextureManager.resolutionByBlockSize);
 
         ps.popPose();
     }
-
-    @FunctionalInterface
-    public interface PSTransformer{
-        Vec3 transform(PoseStack ps, int facing, int blocksize);
-    }
-
-    public static final PSTransformer wallScrollTransformer = (ps, facing, blockSize) -> {
-        ps.translate(-blockSize / 2f, -blockSize / 2f, 1f / 32f);
-        if(facing == 0){
-            return new Vec3(0, 0, -1);
-        }
-        if(facing == 1){
-            return new Vec3(-1, 0, 0);
-        }
-        if(facing == 2){
-            return new Vec3(0, 0, -1);
-        }
-        if(facing == 3){
-            return new Vec3(-1, 0, 0);
-        }
-        return new Vec3(0, 0, -1);
-    };
-
-    public static final PSTransformer floorSlateTransformer = (ps, facing, blockSize) -> {
-        if(facing == 0)
-            ps.translate(0,0,0);
-        if(facing == 1)
-            ps.translate(1,0,0);
-        if(facing == 2)
-            ps.translate(1,0,1);
-        if(facing == 3)
-            ps.translate(0,0,1);
-
-        ps.mulPose(Axis.YP.rotationDegrees(facing*-90));
-        ps.mulPose(Axis.XP.rotationDegrees(90));
-
-        return new Vec3(0, 0, -1);
-    };
-
-    public static final PSTransformer ceilSlateTransformer = (ps, facing, blockSize) -> {
-        if(facing == 0)
-            ps.translate(0,0,0);
-        if(facing == 1)
-            ps.translate(1,0,0);
-        if(facing == 2)
-            ps.translate(1,0,1);
-        if(facing == 3)
-            ps.translate(0,0,1);
-
-        ps.mulPose(Axis.YP.rotationDegrees(facing*-90));
-        ps.mulPose(Axis.XP.rotationDegrees(-90));
-        ps.translate(0,-1,1);
-
-        return new Vec3(0, 0, -1);
-    };
-
-    public static final PSTransformer wallSlateTransformer = (ps, facing, blocksize) -> {
-        ps.mulPose(Axis.ZP.rotationDegrees(180));
-
-        if(facing == 0){
-            ps.translate(0,-1,0);
-            ps.mulPose(Axis.YP.rotationDegrees(180));
-            return new Vec3(0, 0, -1);
-        }
-        if(facing == 1){
-            ps.translate(-1,-1,0);
-            ps.mulPose(Axis.YP.rotationDegrees(270));
-            return new Vec3(-1, 0, 0);
-        }
-        if(facing == 2){
-            ps.translate(-1,-1,1);
-            return new Vec3(0, 0, -1);
-        }
-        if(facing == 3){
-            ps.translate(0,-1,1);
-            ps.mulPose(Axis.YP.rotationDegrees(90));
-            return new Vec3(-1, 0, 0);
-        }
-        return new Vec3(0,0,0);
-    };
-
-    public static final PSTransformer wallBlockTransformer = (ps, facing, blocksize) -> {
-        ps.mulPose(Axis.ZP.rotationDegrees(180));
-
-        if(facing == 0){
-            ps.translate(0,-1,1);
-            ps.mulPose(Axis.YP.rotationDegrees(180));
-            return new Vec3(0, 0, -1);
-        }
-        if(facing == 1){
-            ps.translate(0,-1,0);
-            ps.mulPose(Axis.YP.rotationDegrees(270));
-            return new Vec3(-1, 0, 0);
-        }
-        if(facing == 2){
-            ps.translate(-1,-1,0);
-            return new Vec3(0, 0, -1);
-        }
-        if(facing == 3){
-            ps.translate(-1,-1,1);
-            ps.mulPose(Axis.YP.rotationDegrees(90));
-            return new Vec3(-1, 0, 0);
-        }
-        return new Vec3(0,0,0);
-    };
 }
