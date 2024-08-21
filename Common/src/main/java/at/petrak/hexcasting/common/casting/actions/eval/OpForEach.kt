d0d5c4ead4250f69547b29d7961eba86d1f94b2b
navarchus <kk4atc@gmail.com>
2024-08-20 21:44:54 -0400

    ugly but valid bugfix

--- Common/src/main/java/at/petrak/hexcasting/common/casting/actions/eval/OpForEach.kt
+++ Common/src/main/java/at/petrak/hexcasting/common/casting/actions/eval/OpForEach.kt
@@ -1,5 +1,6 @@
 package at.petrak.hexcasting.common.casting.actions.eval
 
+import at.petrak.hexcasting.api.casting.SpellList
 import at.petrak.hexcasting.api.casting.castables.Action
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
 import at.petrak.hexcasting.api.casting.eval.OperationResult
@@ -7,6 +8,8 @@ import at.petrak.hexcasting.api.casting.eval.vm.CastingImage
 import at.petrak.hexcasting.api.casting.eval.vm.FrameForEach
 import at.petrak.hexcasting.api.casting.eval.vm.SpellContinuation
 import at.petrak.hexcasting.api.casting.getList
+import at.petrak.hexcasting.api.casting.iota.Iota
+import at.petrak.hexcasting.api.casting.mishaps.MishapInvalidIota
 import at.petrak.hexcasting.api.casting.mishaps.MishapNotEnoughArgs
 import at.petrak.hexcasting.common.lib.hex.HexEvalSounds
 
@@ -17,14 +20,28 @@ object OpForEach : Action {
         if (stack.size < 2)
             throw MishapNotEnoughArgs(2, stack.size)
 
-        val instrs = stack.getList(stack.lastIndex - 1)
-        val datums = stack.getList(stack.lastIndex)
+        val newStack = stack.takeLast(2)
+
+        val instrs: SpellList
+        val datums: SpellList
+
+        try {
+            instrs = newStack.getList(newStack.lastIndex - 1)
+        } catch (m: MishapInvalidIota) {
+            throw  MishapInvalidIota.ofType(m.perpetrator, 1, "list")
+        }
+
+        try {
+            datums = newStack.getList(newStack.lastIndex)
+        } catch (m: MishapInvalidIota) {
+            throw  MishapInvalidIota.ofType(m.perpetrator, 0, "list")
+        }
+
         stack.removeLastOrNull()
         stack.removeLastOrNull()
 
         val frame = FrameForEach(datums, instrs, null, mutableListOf())
         val image2 = image.withUsedOp().copy(stack = stack)
-
         return OperationResult(image2, listOf(), continuation.pushFrame(frame), HexEvalSounds.THOTH)
     }
 }
