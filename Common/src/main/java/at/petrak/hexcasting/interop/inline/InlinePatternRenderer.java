0b284d773a97c2e84eb50355ca668935f67862d3
SamsTheNerd <sam@samsthenerd.com>
2024-09-28 23:17:43 -0400

    make inline patterns glow nice on signs

--- Common/src/main/java/at/petrak/hexcasting/interop/inline/InlinePatternRenderer.java
+++ Common/src/main/java/at/petrak/hexcasting/interop/inline/InlinePatternRenderer.java
@@ -1,7 +1,9 @@
 package at.petrak.hexcasting.interop.inline;
 
 import at.petrak.hexcasting.client.render.*;
+import com.samsthenerd.inline.api.client.GlowHandling;
 import com.samsthenerd.inline.api.client.InlineRenderer;
+import com.samsthenerd.inline.impl.InlineStyle;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Style;
 import net.minecraft.resources.ResourceLocation;
@@ -19,7 +21,7 @@ public class InlinePatternRenderer implements InlineRenderer<InlinePatternData> 
                     PatternSettings.AxisAlignment.CENTER, PatternSettings.AxisAlignment.CENTER_FIT, 4.0, 0, 0),
             PatternSettings.StrokeSettings.fromStroke(1.0),
             new PatternSettings.ZappySettings(10, 0, 0, 0,
-                    PatternSettings.ZappySettings.READABLE_OFFSET, 1f)
+                    PatternSettings.ZappySettings.READABLE_OFFSET, 0.7f)
     ){
         @Override
         public double getOuterWidth(double scale){
@@ -30,14 +32,39 @@ public class InlinePatternRenderer implements InlineRenderer<InlinePatternData> 
         }
     };
 
+    public static final PatternSettings INLINE_SETTINGS_GLOWY = new PatternSettings("inlineglowy",
+            new PatternSettings.PositionSettings(1.0, 11.0, 0, 0.5,
+                    PatternSettings.AxisAlignment.CENTER, PatternSettings.AxisAlignment.CENTER_FIT, 4.0, 0, 0),
+            new PatternSettings.StrokeSettings(1, 3, 0.8 * 1 * 2.0 / 5.0, 0.4 * 1 * 2.0 / 5.0),
+            INLINE_SETTINGS.zapSets
+    ){
+        @Override
+        public double getInnerWidth(double scale){
+            if(scale >= 1) return 1;
+            if(scale >= 0.75) return 0.75;
+            if(scale >= 0.5) return 0.5;
+            return 0.25;
+        }
+    };
+
+    @Override
+    public GlowHandling getGlowPreference(InlinePatternData forData) {
+        return new GlowHandling.None();
+    }
+
     public static final int INLINE_TEXTURE_RES = 16; // 128px so it looks good and pretty on up close signs and whatnot
 
     public int render(InlinePatternData data, GuiGraphics drawContext, int index, Style style, int codepoint, TextRenderingContext trContext){
+        if(trContext.isGlowy()) return charWidth(data, style, codepoint);
+        int glowyParentColor = ((InlineStyle) style).getComponent(InlineStyle.GLOWY_PARENT_COMP);
+        boolean isGlowy = glowyParentColor != -1;
         drawContext.pose().pushPose();
-        drawContext.pose().translate(0f, -0.5f, 0f);
+        drawContext.pose().translate(isGlowy ? -1f : 0, isGlowy ? -1.5f : -0.5f, 0f);
         int color = trContext.usableColor();
         PatternRenderer.renderPattern(data.pattern, drawContext.pose(), new PatternRenderer.WorldlyBits(drawContext.bufferSource(), trContext.light(), null),
-                INLINE_SETTINGS, PatternColors.singleStroke(color), 0, INLINE_TEXTURE_RES);
+                isGlowy ? INLINE_SETTINGS_GLOWY : INLINE_SETTINGS,
+                isGlowy ? new PatternColors(color, 0xFF_000000 | glowyParentColor) : PatternColors.singleStroke(color),
+                0, INLINE_TEXTURE_RES);
 
         drawContext.pose().popPose();
         return charWidth(data, style, codepoint);
