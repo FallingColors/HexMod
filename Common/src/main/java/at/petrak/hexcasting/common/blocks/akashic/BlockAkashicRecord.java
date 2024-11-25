1c3e4db6a1b326e57e5ec2480d34fa1312deade9
SamsTheNerd <sam@samsthenerd.com>
2024-11-24 20:23:37 -0500

    add blocky chroniclers support for slates, akashic bookshelf labels, akashic record labels

--- Common/src/main/java/at/petrak/hexcasting/common/blocks/akashic/BlockAkashicRecord.java
+++ Common/src/main/java/at/petrak/hexcasting/common/blocks/akashic/BlockAkashicRecord.java
@@ -1,7 +1,11 @@
 package at.petrak.hexcasting.common.blocks.akashic;
 
+import at.petrak.hexcasting.api.HexAPI.IBlockyIotaProvider;
+import at.petrak.hexcasting.api.addldata.ADIotaHolder;
 import at.petrak.hexcasting.api.casting.iota.Iota;
 import at.petrak.hexcasting.api.casting.iota.IotaType;
+import at.petrak.hexcasting.api.casting.iota.ListIota;
+import at.petrak.hexcasting.api.casting.iota.PatternIota;
 import at.petrak.hexcasting.api.casting.math.HexPattern;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
@@ -9,7 +13,10 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import org.jetbrains.annotations.Nullable;
 
-public class BlockAkashicRecord extends Block {
+import java.util.ArrayList;
+import java.util.List;
+
+public class BlockAkashicRecord extends Block implements IBlockyIotaProvider {
     public BlockAkashicRecord(Properties p_49795_) {
         super(p_49795_);
     }
@@ -61,4 +68,19 @@ public class BlockAkashicRecord extends Block {
     }
 
     // TODO get comparators working again and also cache the number of iotas somehow?
+
+    @Override
+    public ADIotaHolder getIotaHolder(ServerLevel level, BlockPos recPos) {
+        List<Iota> labels = new ArrayList<>();
+        AkashicFloodfiller.floodFillFor(recPos, level,
+            (pos, bs, world) -> {
+                if(world.getBlockEntity(pos) instanceof BlockEntityAkashicBookshelf tile
+                && tile.getPattern() != null
+                ){
+                    labels.add(new PatternIota(tile.getPattern()));
+                }
+                return false;
+            });
+        return ADIotaHolder.ofStatic(new ListIota(labels));
+    }
 }
