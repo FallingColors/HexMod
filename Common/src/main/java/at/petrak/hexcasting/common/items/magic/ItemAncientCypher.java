12d9d4cc3fb82a5cd50c341390fb59b4610072d1
Robotgiggle <purplemrb12@gmail.com>
2024-12-05 02:16:27 -0500

    Basic item implementation

---
+++ Common/src/main/java/at/petrak/hexcasting/common/items/magic/ItemAncientCypher.java
@@ -0,0 +1,7 @@
+package at.petrak.hexcasting.common.items.magic;
+
+public class ItemAncientCypher extends ItemCypher {
+    public ItemAncientCypher(Properties pProperties) {
+        super(pProperties);
+    }
+}
