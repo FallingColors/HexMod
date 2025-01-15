e1b9a7c12143af1b0ae596801f5701813e02015f
Robotgiggle <purplemrb12@gmail.com>
2025-01-15 14:09:03 -0500

    Loot tables for guaranteed scroll/cypher

--- Common/src/main/java/at/petrak/hexcasting/common/loot/HexLootHandler.java
+++ Common/src/main/java/at/petrak/hexcasting/common/loot/HexLootHandler.java
@@ -47,6 +47,8 @@ public class HexLootHandler {
         // if you manage to find one of these things you deserve a lot of scrolls
         new ScrollInjection(new ResourceLocation("minecraft", "chests/woodland_mansion"), 5),
         new ScrollInjection(new ResourceLocation("minecraft", "chests/stronghold_library"), 5)
+
+        // you can use the table hexcasting:random_scroll to get exactly 1 scroll and nothing else
     );
 
     public static final ImmutableList<ResourceLocation> DEFAULT_LORE_INJECTS = ImmutableList.of(
@@ -71,6 +73,7 @@ public class HexLootHandler {
         new ResourceLocation("minecraft", "chests/desert_pyramid"),
         new ResourceLocation("minecraft", "chests/ancient_city"),
         new ResourceLocation("minecraft", "chests/nether_bridge")
+        // you can use the table hexcasting:random_cypher to get exactly 1 cypher and nothing else
     );
 
     public static final ImmutableList<List<String>> DEFAULT_LOOT_HEXES = ImmutableList.of(
