8df0b457f68dc6852685d26ebb1d711a66c78a29
Robotgiggle <88736742+Robotgiggle@users.noreply.github.com>
2026-03-07 02:11:24 -0500

    Fix mishap for no placeable item in hotbar

---
+++ Common/src/main/java/at/petrak/hexcasting/api/casting/mishaps/MishapBadHotbarItem.kt
@@ -0,0 +1,31 @@
+package at.petrak.hexcasting.api.casting.mishaps
+
+import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
+import at.petrak.hexcasting.api.casting.iota.Iota
+import at.petrak.hexcasting.api.pigment.FrozenPigment
+import at.petrak.hexcasting.api.utils.asTranslatedComponent
+import net.minecraft.network.chat.Component
+import net.minecraft.world.InteractionHand
+import net.minecraft.world.item.DyeColor
+import net.minecraft.world.item.ItemStack
+
+class MishapBadHotbarItem(val item: ItemStack?, val wanted: Component) : Mishap() {
+    override fun accentColor(ctx: CastingEnvironment, errorCtx: Context): FrozenPigment =
+        dyeColor(DyeColor.BROWN)
+
+    override fun execute(env: CastingEnvironment, errorCtx: Context, stack: MutableList<Iota>) {
+        env.mishapEnvironment.dropHeldItems()
+    }
+
+    override fun errorMessage(ctx: CastingEnvironment, errorCtx: Context) = if (item?.isEmpty == false)
+        error("bad_item.hotbar", wanted, item.count, item.displayName)
+    else
+        error("no_item.hotbar", wanted)
+
+    companion object {
+        @JvmStatic
+        fun of(item: ItemStack?, stub: String, vararg args: Any): MishapBadHotbarItem {
+            return MishapBadHotbarItem(item, "hexcasting.mishap.bad_item.$stub".asTranslatedComponent(*args))
+        }
+    }
+}
--- Common/src/main/java/at/petrak/hexcasting/common/casting/actions/spells/OpPlaceBlock.kt
+++ Common/src/main/java/at/petrak/hexcasting/common/casting/actions/spells/OpPlaceBlock.kt
@@ -7,7 +7,7 @@ import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
 import at.petrak.hexcasting.api.casting.getBlockPos
 import at.petrak.hexcasting.api.casting.iota.Iota
 import at.petrak.hexcasting.api.casting.mishaps.MishapBadBlock
-import at.petrak.hexcasting.api.casting.mishaps.MishapBadOffhandItem
+import at.petrak.hexcasting.api.casting.mishaps.MishapBadHotbarItem
 import at.petrak.hexcasting.api.misc.MediaConstants
 import at.petrak.hexcasting.xplat.IXplatAbstractions
 import net.minecraft.core.BlockPos
@@ -41,7 +41,7 @@ object OpPlaceBlock : SpellAction {
         val itemUseCtx = env
             .queryForMatchingStack { it.item is BlockItem }
             ?.let { UseOnContext(env.world, env.castingEntity as? ServerPlayer, env.castingHand, it, blockHit) }
-            ?: throw MishapBadOffhandItem.of(ItemStack.EMPTY, "placeable")
+            ?: throw MishapBadHotbarItem.of(ItemStack.EMPTY, "placeable")
         val placeContext = BlockPlaceContext(itemUseCtx)
 
         val worldState = env.world.getBlockState(pos)
