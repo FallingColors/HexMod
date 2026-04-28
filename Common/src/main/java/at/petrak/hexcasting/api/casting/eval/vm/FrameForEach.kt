a1c63cd177bbb58e18fd1fbf25463fb4c2fb1506
Aly <me@s5.pm>
2026-04-27 22:27:55 -0600

    remove useless .toList calls

--- Common/src/main/java/at/petrak/hexcasting/api/casting/eval/vm/FrameForEach.kt
+++ Common/src/main/java/at/petrak/hexcasting/api/casting/eval/vm/FrameForEach.kt
@@ -44,7 +44,7 @@ data class FrameForEach(
     /** When halting, we add the stack state at halt to the stack accumulator, then return the original pre-Thoth stack, plus the accumulator. */
     override fun breakDownwards(stack: List<Iota>): Pair<Boolean, List<Iota>> {
         val newStack = baseStack?.toMutableList() ?: mutableListOf()
-        newStack.add(ListIota(immutableAcc.appendedAll(stack).toList()))
+        newStack.add(ListIota(immutableAcc.appendedAll(stack)))
         return true to newStack
     }
 
@@ -74,7 +74,7 @@ data class FrameForEach(
             Triple(data.car, harness.image.withUsedOp(), cont2)
         } else {
             // Else, dump our final list onto the stack.
-            Triple(ListIota(immutableAcc.toList()), harness.image, continuation)
+            Triple(ListIota(immutableAcc), harness.image, continuation)
         }
         val tStack = stack.toMutableList()
         tStack.add(stackTop)
