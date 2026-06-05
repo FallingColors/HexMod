d01686392645823f4d394dd3061a9d18607ec58d
IridescentVoid <265486018+iridescentvoid@users.noreply.github.com>
2026-06-05 10:22:59 -0700

    Allow overwriting akashic libraries

--- Common/src/main/java/at/petrak/hexcasting/common/blocks/akashic/BlockEntityAkashicBookshelf.java
+++ Common/src/main/java/at/petrak/hexcasting/common/blocks/akashic/BlockEntityAkashicBookshelf.java
@@ -44,12 +44,13 @@ public class BlockEntityAkashicBookshelf extends HexBlockEntity {
         this.pattern = pattern;
         this.iotaTag = IotaType.serialize(iota);
 
+        var oldBs = this.getBlockState();
         if (previouslyEmpty) {
-            var oldBs = this.getBlockState();
             var newBs = oldBs.setValue(BlockAkashicBookshelf.HAS_BOOKS, true);
-            this.level.setBlock(this.getBlockPos(), newBs, 3);
             this.level.sendBlockUpdated(this.getBlockPos(), oldBs, newBs, 3);
+            this.level.setBlock(this.getBlockPos(), newBs, 3);
         } else {
+            this.level.sendBlockUpdated(this.getBlockPos(), oldBs, oldBs, 3);
             this.setChanged();
         }
     }
