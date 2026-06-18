382c02f665fa1cfe9a1624d4d8b5eeb462639a3b
Robotgiggle <88736742+Robotgiggle@users.noreply.github.com>
2026-06-17 20:44:26 -0400

    Make escaper patterns increment the op count

--- Common/src/main/java/at/petrak/hexcasting/common/casting/actions/escaping/OpEscape.kt
+++ Common/src/main/java/at/petrak/hexcasting/common/casting/actions/escaping/OpEscape.kt
@@ -12,14 +12,14 @@ import at.petrak.hexcasting.common.lib.hex.HexEvalSounds
 
 object OpEscape : Action {
     override fun operate(env: CastingEnvironment, image: CastingImage, continuation: SpellContinuation): OperationResult {
-        val image2 = image.copy(
+        val image2 = image.withUsedOp().copy(
             escapeNext = true
         )
         return OperationResult(image2, listOf(), continuation, HexEvalSounds.NORMAL_EXECUTE)
     }
 
     override fun operateInParens(env: CastingEnvironment, image: CastingImage, continuation: SpellContinuation, thisIota: Iota): ParenthesizedOperationResult {
-        val image2 = image.copy(
+        val image2 = image.withUsedOp().copy(
             escapeNext = true
         )
         return ParenthesizedOperationResult(image2, listOf(), continuation, HexEvalSounds.NORMAL_EXECUTE, ResolvedPatternType.EVALUATED)
