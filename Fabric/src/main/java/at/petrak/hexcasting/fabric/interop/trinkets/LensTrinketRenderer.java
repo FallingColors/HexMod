8838cf66f0bcad95b1e90d87b94dd577570f2d50
Skye <skye@is-quite.gay>
2026-07-01 15:54:51 +0900

    Fix scrying lens and related attributes, replace Accessories with Trinkets/Curios (#1185)
    we pull in Accessories and don't even take advantage of the fact it's
    multiloader. we already have a perfectly functional xplat
    curios/trinkets abstraction, just use it instead of using that
    abstraction to do curios/accessories for some reason
    
    oh also I rolled some minor fixes that prevented the project from
    working at all on my puter into this. I can pull them out if needed
    
    changes tested working on both NeoForge and Fabric, with Curios and
    Trinkets respectively.
    
    fixes #1180, #1096, and #1123
    
    conflicts with #1184

--- Fabric/src/main/java/at/petrak/hexcasting/fabric/interop/trinkets/LensTrinketRenderer.java
+++ Fabric/src/main/java/at/petrak/hexcasting/fabric/interop/trinkets/LensTrinketRenderer.java
@@ -1,17 +1,17 @@
-package at.petrak.hexcasting.fabric.interop.accessories;
+package at.petrak.hexcasting.fabric.interop.trinkets;
 
 import at.petrak.hexcasting.common.lib.HexItems;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.math.Axis;
-import io.wispforest.accessories.api.client.AccessoryRenderer;
-import io.wispforest.accessories.api.slot.SlotReference;
+import dev.emi.trinkets.api.SlotReference;
+import dev.emi.trinkets.api.client.TrinketRenderer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.PlayerModel;
+import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 
@@ -19,17 +19,18 @@ import net.minecraft.world.item.ItemStack;
  * @author WireSegal
  * Created at 9:50 AM on 7/25/22.
  */
-public class LensAccessoryRenderer implements AccessoryRenderer {
+public class LensTrinketRenderer implements TrinketRenderer {
     @Override
-    @SuppressWarnings("unchecked")
-    public <M extends LivingEntity> void render(ItemStack stack, SlotReference slotReference, PoseStack matrices, EntityModel<M> model, MultiBufferSource multiBufferSource, int light, float v, float v1, float v2, float v3, float v4, float v5) {
+    public void render(ItemStack stack, SlotReference slotReference, EntityModel<? extends LivingEntity> model, PoseStack matrices, MultiBufferSource multiBufferSource, int light, LivingEntity entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
         if (stack.is(HexItems.SCRYING_LENS) &&
-                model instanceof PlayerModel playerModel && slotReference.entity() instanceof Player) {
+                model instanceof PlayerModel playerModel && entity instanceof AbstractClientPlayer player) {
 
             // from https://github.com/Creators-of-Create/Create/blob/ee33823ed0b5084af10ed131a1626ce71db4c07e/src/main/java/com/simibubi/create/compat/curios/GogglesCurioRenderer.java
 
             // Translate and rotate with our head
             matrices.pushPose();
+            TrinketRenderer.followBodyRotations(entity, playerModel);
+            TrinketRenderer.translateToFace(matrices, playerModel, player, headYaw, headPitch);
 
             // Translate and scale to our head
             matrices.translate(0, 0, 0.3);
