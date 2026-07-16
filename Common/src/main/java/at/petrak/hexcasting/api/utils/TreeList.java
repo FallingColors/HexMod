1310a4bb84f601c686f2593d6495e6426e69d1ba
Aly <me@s5.pm>
2026-07-15 22:50:42 -0600

    document head, tail, init, last with snippets and take/drop(right) with return info

--- Common/src/main/java/at/petrak/hexcasting/api/utils/TreeList.java
+++ Common/src/main/java/at/petrak/hexcasting/api/utils/TreeList.java
@@ -464,26 +464,58 @@ public sealed abstract class TreeList<A> extends AbstractList<A> implements Rand
         } else return new TreeListBuilder<A>().initFrom(this).addAll(suffix).result();
     }
 
+    /**
+     * @param n the number of elements to keep
+     * @return a new TreeList containing only the first {@code n} elements of this TreeList
+     */
     public final TreeList<A> take(int n) {
         return this.slice(0, n);
     }
 
+    /**
+     * @param n the number of elements to discard
+     * @return a new TreeList containing all but the first {@code n} elements of this TreeList
+     */
     public final TreeList<A> drop(int n) {
         return this.slice(n, this.length());
     }
 
+    /**
+     * @param n the number of elements to keep
+     * @return a new TreeList containing only the last {@code n} elements of this TreeList
+     */
     public final TreeList<A> takeRight(int n) {
         return this.slice(this.length() - Math.max(n, 0), this.length());
     }
 
+    /**
+     * @param n the number of elements to discard
+     * @return a new TreeList containing all but the last {@code n} elements of this TreeList
+     */
     public final TreeList<A> dropRight(int n) {
         return this.slice(0, this.length() - Math.max(n, 0));
     }
 
+    /**
+     * {@snippet :
+     *     TreeList<String> x = TreeList.from(List.of("a", "b", "c"));
+     *     assert TreeList.from(List.of("b", "c")).equals(x.tail());
+     * }
+     *
+     * @return a new TreeList containing all the elements from this TreeList except for the first one
+     */
     public TreeList<A> tail() {
         return this.slice(1, this.length());
     }
 
+    /**
+     * {@snippet :
+     *     TreeList<String> x = TreeList.from(List.of("a", "b", "c"));
+     *     assert TreeList.from(List.of("a", "b")).equals(x.init());
+     * }
+     *
+     * @return a new TreeList containing all the elements from this TreeList except for the last one
+     */
     public TreeList<A> init() {
         return this.slice(0, this.length() - 1);
     }
@@ -498,11 +530,27 @@ public sealed abstract class TreeList<A> extends AbstractList<A> implements Rand
     /** Length of all slices up to and including index */
     abstract int treeListSlicePrefixLength(int idx);
 
+    /**
+     * {@snippet :
+     *     TreeList<String> x = TreeList.from(List.of("a", "b", "c"));
+     *     assert "a".equals(x.head());
+     * }
+     *
+     * @return the first element of this TreeList
+     */
     public final A head() {
         if(this.prefix1.length == 0) throw new NoSuchElementException("empty.head");
         else return (A) this.prefix1[0];
     }
 
+    /**
+     * {@snippet :
+     *     TreeList<String> x = TreeList.from(List.of("a", "b", "c"));
+     *     assert "c".equals(x.last());
+     * }
+     *
+     * @return the last element of this TreeList
+     */
     public A last() {
         return (A) this.prefix1[this.prefix1.length - 1];
     }
