204253db8b15a231f68ab2c477681e1529e85f83
Florian Meyer <flommeyer@gmail.com>
2026-07-27 00:01:30 +0200

    fix: IotaPredicates calling IotaType holders too early

--- Common/src/main/java/at/petrak/hexcasting/common/casting/arithmetic/BitwiseSetArithmetic.kt
+++ Common/src/main/java/at/petrak/hexcasting/common/casting/arithmetic/BitwiseSetArithmetic.kt
@@ -35,10 +35,10 @@ object BitwiseSetArithmetic : Arithmetic {
         else -> throw InvalidOperatorException("$pattern is not a valid operator in Arithmetic $this.")
     }
 
-    private fun make1(op: LongUnaryOperator): OperatorUnary = OperatorUnary(DoubleArithmetic.ACCEPTS)
+    private fun make1(op: LongUnaryOperator): OperatorUnary = OperatorUnary(DoubleArithmetic.ACCEPTS())
         { i: Iota -> DoubleIota(op.applyAsLong(downcast(i, DOUBLE.get()).double.roundToLong()).toDouble()) }
 
-    private fun make2(op: LongBinaryOperator): OperatorBinary = OperatorBinary(DoubleArithmetic.ACCEPTS)
+    private fun make2(op: LongBinaryOperator): OperatorBinary = OperatorBinary(DoubleArithmetic.ACCEPTS())
         { i: Iota, j: Iota -> DoubleIota(
                 op.applyAsLong(
                     downcast(i, DOUBLE.get()).double.roundToLong(),
