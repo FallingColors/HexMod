23a5203a35174757e04bb7357f309fc5e2da0803
flmeyer <florian.meyer@capgemini.com>
2026-08-05 11:15:14 +0200

    fix: fabric spellbook scrolling

--- Fabric/src/main/java/at/petrak/hexcasting/fabric/mixin/client/FabricMouseHandlerMixin.java
+++ Fabric/src/main/java/at/petrak/hexcasting/fabric/mixin/client/FabricMouseHandlerMixin.java
@@ -1,17 +1,17 @@
 package at.petrak.hexcasting.fabric.mixin.client;
 
 import at.petrak.hexcasting.fabric.event.MouseScrollCallback;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.MouseHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(MouseHandler.class)
 public class FabricMouseHandlerMixin {
-    @Inject(method = "onScroll", cancellable = true, locals = LocalCapture.CAPTURE_FAILSOFT, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;isSpectator()Z"))
-    private void onScroll(long winptr, double xOff, double yOff, CallbackInfo ci, double delta) {
+    @Inject(method = "onScroll", cancellable = true, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/player/LocalPlayer;isSpectator()Z"))
+    private void onScroll(long l, double d, double e, CallbackInfo ci, @Local(name = "n") int delta) {
         var cancel = MouseScrollCallback.EVENT.invoker().interact(delta);
         if (cancel) {
             ci.cancel();
