FallingColors/HexMod

commit 16cf0767c5937bb6bef69e4e295ef13babf2ccd0

Hide arrow on argument-less pattern pages (#1118)
Resolves #566

I don't quite like duplicating the `#text#` component with inverse guard
clauses, but the alternative would be putting the y height in the
PatternProcessor which feels worse.

---------

Co-authored-by: Robotgiggle <88736742+Robotgiggle@users.noreply.github.com>

Common/src/main/java/at/petrak/hexcasting/interop/patchouli/PatternProcessor.java
@@ -8,18 +8,24 @@ import vazkii.patchouli.api.IVariableProvider;
 
 public class PatternProcessor implements IComponentProcessor {
     private String translationKey;
+    private boolean hasArgs = false;
 
     @Override
     public void setup(Level level, IVariableProvider vars) {
-        if (vars.has("header"))
+        if (vars.has("header")) {
             translationKey = vars.get("header").asString();
-        else {
+        } else {
             IVariable key = vars.get("op_id");
             String opName = key.asString();
 
             String prefix = "hexcasting.action.";
             boolean hasOverride = I18n.exists(prefix + "book." + opName);
             translationKey = prefix + (hasOverride ? "book." : "") + opName;
+
+            if (vars.has("input") && !vars.get("input").asString().isEmpty())
+                hasArgs = true;
+            else if (vars.has("output") && !vars.get("output").asString().isEmpty())
+                hasArgs = true;
         }
     }
 
@@ -27,6 +33,8 @@ public class PatternProcessor implements IComponentProcessor {
     public IVariable process(Level level, String key) {
         if (key.equals("translation_key")) {
             return IVariable.wrap(translationKey);
+        } else if (key.equals("has_signature")) {
+            return IVariable.wrap(hasArgs);
         }
 
         return null;

commit 9d1f57f76e62ec43bb0eca15c7bf565b194c0b8f

fix: properly track argument presence in the hex notebook (#1242)
Regression from #1118 

Prior to this fix, pages that had their header overwritten would not
have their signatures visible on the book page. This affects Additive
Distillation in the lists entry, and probably other entries.

Common/src/main/java/at/petrak/hexcasting/interop/patchouli/PatternProcessor.java
@@ -21,12 +21,12 @@ public class PatternProcessor implements IComponentProcessor {
             String prefix = "hexcasting.action.";
             boolean hasOverride = I18n.exists(prefix + "book." + opName);
             translationKey = prefix + (hasOverride ? "book." : "") + opName;
-
-            if (vars.has("input") && !vars.get("input").asString().isEmpty())
-                hasArgs = true;
-            else if (vars.has("output") && !vars.get("output").asString().isEmpty())
-                hasArgs = true;
         }
+
+        if (vars.has("input") && !vars.get("input").asString().isEmpty())
+            hasArgs = true;
+        else if (vars.has("output") && !vars.get("output").asString().isEmpty())
+            hasArgs = true;
     }
 
     @Override