16a6193c070f1f3d213aede9fb6feeffe5fa2738
object-Object <object@objectobject.ca>
2026-08-19 05:36:51 +0000

    Deploying to gh-pages from @ FallingColors/HexMod@60086cc6fdae84b75fb7a508912f15374a0f3dc8 🚀

--- docs/v/latest/main/es_es/hexcasting_render.js
+++ docs/v/latest/main/es_es/hexcasting_render.js
@@ -345,15 +345,15 @@ export function load_palette(name) {
   }
 }
 const render_lang = {
-  "animated": "animated",
-  "monocolor": "monocolor",
-  "gradient": "gradient",
-  "segment": "segment",
+  "animado": "animated",
+  "monocromo": "monocolor",
+  "gradiente": "gradient",
+  "segmento": "segment",
 }
 const palette_lang = {
-  "default": "default",
+  "por defecto": "default",
   "turbo": "turbo",
-  "dark2": "dark2",
+  "oscuro2": "dark2",
   "tab10": "tab10",
 };
 //adds all of the options to the menus (renderer and palette menus)
