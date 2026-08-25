3c5e8071ef29c4255056d3f88e97eae41ee7623c
Robotgiggle <88736742+Robotgiggle@users.noreply.github.com>
2026-08-25 19:42:44 -0400

    Initial armor stats (may need rebalancing)
    # Conflicts:
    #	Common/src/main/java/at/petrak/hexcasting/common/lib/HexItems.java

--- Common/src/main/java/at/petrak/hexcasting/common/lib/HexItems.java
+++ Common/src/main/java/at/petrak/hexcasting/common/lib/HexItems.java
@@ -1,5 +1,6 @@
 package at.petrak.hexcasting.common.lib;
 
+import at.petrak.hexcasting.api.HexAPI;
 import at.petrak.hexcasting.api.casting.ActionRegistryEntry;
 import at.petrak.hexcasting.api.misc.MediaConstants;
 import at.petrak.hexcasting.api.mod.HexTags;
@@ -76,11 +77,6 @@ public class HexItems {
             IXplatAbstractions.INSTANCE.addEquipSlotFabric(EquipmentSlot.HEAD)
                     .stacksTo(1).attributes(ItemLens.MODIFIERS)));
 
-    public static final Supplier<ItemRobes> ROBES_HOOD = make("robes/hood", () -> new ItemRobes(ArmorItem.Type.HELMET, unstackable().rarity(Rarity.UNCOMMON)));
-    public static final Supplier<ItemRobes> ROBES_TUNIC = make("robes/tunic", () -> new ItemRobes(ArmorItem.Type.CHESTPLATE, unstackable().rarity(Rarity.UNCOMMON)));
-    public static final Supplier<ItemRobes> ROBES_LEGS = make("robes/legs", () -> new ItemRobes(ArmorItem.Type.LEGGINGS, unstackable().rarity(Rarity.UNCOMMON)));
-    public static final Supplier<ItemRobes> ROBES_BOOTS = make("robes/boots", () -> new ItemRobes(ArmorItem.Type.BOOTS, unstackable().rarity(Rarity.UNCOMMON)));
-
     public static final Supplier<ItemAbacus> ABACUS = make("abacus", () -> new ItemAbacus(unstackable()));
     public static final Supplier<ItemThoughtKnot> THOUGHT_KNOT = make("thought_knot", () -> new ItemThoughtKnot(unstackable()));
     public static final Supplier<ItemFocus> FOCUS = make("focus", () -> new ItemFocus(unstackable()));
@@ -107,6 +103,35 @@ public class HexItems {
     );
 
     public static final Supplier<Item> NEURAL_FIBER = make("neural_fiber", () -> new Item(props().rarity(Rarity.UNCOMMON)));
+
+    public static final Supplier<ItemRobes> ROBES_HOOD = make("robes/hood", () ->
+        new ItemRobes(ArmorItem.Type.HELMET, unstackable()
+            .rarity(Rarity.UNCOMMON)
+            .durability(270)
+            .attributes(ItemRobes.HOOD_MODIFIERS)
+        )
+    );
+    public static final Supplier<ItemRobes> ROBES_TUNIC = make("robes/tunic", () ->
+        new ItemRobes(ArmorItem.Type.CHESTPLATE, unstackable()
+            .rarity(Rarity.UNCOMMON)
+            .durability(400)
+            .attributes(ItemRobes.TUNIC_MODIFIERS)
+        )
+    );
+    public static final Supplier<ItemRobes> ROBES_LEGS = make("robes/legs", () ->
+        new ItemRobes(ArmorItem.Type.LEGGINGS, unstackable()
+            .rarity(Rarity.UNCOMMON)
+            .durability(350)
+            .attributes(ItemRobes.LEGS_MODIFIERS)
+        )
+    );
+    public static final Supplier<ItemRobes> ROBES_BOOTS = make("robes/boots", () ->
+        new ItemRobes(ArmorItem.Type.BOOTS, unstackable()
+            .rarity(Rarity.UNCOMMON)
+            .durability(315)
+            .attributes(ItemRobes.BOOTS_MODIFIERS)
+        )
+    );
 
     public static final Supplier<ItemScroll> SCROLL_SMOL = make("scroll_small", () -> new ItemScroll(props(), 1));
     public static final Supplier<ItemScroll> SCROLL_MEDIUM = make("scroll_medium", () -> new ItemScroll(props(), 2));
