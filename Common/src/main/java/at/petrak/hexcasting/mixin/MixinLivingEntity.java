c17fec43b04743ef7644ce26cd9fde234b71cfc2
Robotgiggle <purplemrb12@gmail.com>
2026-08-29 22:08:20 -0400

    Make mobs drop fiber+dust when crystallized

--- Common/src/main/java/at/petrak/hexcasting/mixin/MixinLivingEntity.java
+++ Common/src/main/java/at/petrak/hexcasting/mixin/MixinLivingEntity.java
@@ -1,24 +1,39 @@
 package at.petrak.hexcasting.mixin;
 
+import at.petrak.hexcasting.api.HexAPI;
+import at.petrak.hexcasting.common.lib.HexItems;
+import at.petrak.hexcasting.common.lib.HexMobEffects;
+import at.petrak.hexcasting.common.misc.HexMobEffect;
 import at.petrak.hexcasting.xplat.IXplatAbstractions;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.Brain;
+import net.minecraft.world.entity.npc.Villager;
+import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-// Nuke the brain at the source
+
 @Mixin(LivingEntity.class)
 public abstract class MixinLivingEntity {
-    @Inject(method = "getBrain", at = @At("RETURN"))
-    private void removeBrain(CallbackInfoReturnable<Brain<?>> cir) {
+    @Inject(method = "dropAllDeathLoot", require = 1, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/LivingEntity;dropFromLootTable(Lnet/minecraft/world/damagesource/DamageSource;Z)V"))
+    private void dropCrystallizedLoot(CallbackInfo ci) {
         var self = (LivingEntity) (Object) this;
-        if (self instanceof Mob mob && IXplatAbstractions.INSTANCE.isBrainswept(mob)) {
-            var brain = cir.getReturnValue();
-            brain.removeAllBehaviors();
-            cir.setReturnValue(brain);
+        var rand = self.getRandom();
+        if (self.hasEffect(HexMobEffects.CRYSTALLIZED)) {
+            int fibers = rand.nextIntBetweenInclusive(1,3);
+            int dust = rand.nextIntBetweenInclusive(4,7);
+            int extra = 0;
+            if (self instanceof Villager vill) {
+                int scaledLvl = vill.getVillagerData().getLevel() - 2;
+                if (rand.nextIntBetweenInclusive(0, 1) <= scaledLvl) extra++;
+                if (rand.nextIntBetweenInclusive(2, 3) <= scaledLvl) extra++;
+            }
+            self.spawnAtLocation(new ItemStack(HexItems.NEURAL_FIBER.get(), fibers + extra));
+            self.spawnAtLocation(new ItemStack(HexItems.AMETHYST_DUST.get(), dust + extra));
         }
     }
 }
