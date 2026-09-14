226c63fc80b67a45e1b7fa23c76fccbad53684ef
Robotgiggle <purplemrb12@gmail.com>
2026-09-14 15:09:34 -0400

    Mishap for lack-of-shelf-space

---
+++ Common/src/main/java/at/petrak/hexcasting/api/casting/mishaps/MishapNoShelfSpace.kt
@@ -0,0 +1,21 @@
+package at.petrak.hexcasting.api.casting.mishaps
+
+import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
+import at.petrak.hexcasting.api.casting.iota.Iota
+import at.petrak.hexcasting.api.pigment.FrozenPigment
+import at.petrak.hexcasting.api.utils.TreeList
+import net.minecraft.core.BlockPos
+import net.minecraft.world.item.DyeColor
+
+class MishapNoShelfSpace(val pos: BlockPos) : Mishap() {
+    override fun accentColor(env: CastingEnvironment, errorCtx: Context): FrozenPigment =
+        dyeColor(DyeColor.PURPLE)
+
+    override fun execute(env: CastingEnvironment, errorCtx: Context, stack: TreeList<Iota>): TreeList<Iota> {
+        env.mishapEnvironment.removeXp(20)
+        return stack
+    }
+
+    override fun errorMessage(env: CastingEnvironment, errorCtx: Context) =
+        error("no_shelf_space", pos.toShortString())
+}
